c9974fbbc93e699892fdf94d0f7e8bc1f3585684
Aghalarova-Samira <aqalarova0@gmail.com>
2022-03-17 18:44:34 +0400

    my first commytit

--- api/index.js
+++ api/index.js
@@ -57,7 +57,7 @@ app.use(ProductRouthe)
 app.use(BasketRouthe)
 
 
-app.use(express.static(path.join(__dirname, "/client")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
