e60457c83a7ff2abdbc416355252d250f340f5dc
Aghalarova-Samira <aqalarova0@gmail.com>
2022-03-17 18:35:05 +0400

    my first commsait

--- api/index.js
+++ api/index.js
@@ -68,3 +68,22 @@ app.get('*', (req, res) => {
 app.listen(process.env.PORT || 4000, () => {
     console.log("Server Has Started");
   });
+
+
+
+  // {
+  //   "short_name": "React App",
+  //   "name": "Create React App Sample",
+  //   "icons": [
+  //     {
+  //       "src": "favicon.ico",
+  //       "sizes": "64x64 32x32 24x24 16x16",
+  //       "type": "image/x-icon"
+  //     }
+  
+  //   ],
+  //   "start_url": ".",
+  //   "display": "standalone",
+  //   "theme_color": "#000000",
+  //   "background_color": "#ffffff"
+  // }
