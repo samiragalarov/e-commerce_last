1e38c384af22287a1f3bd24b5bbae2fafa0c135d
Aghalarova-Samira <aqalarova0@gmail.com>
2022-03-17 18:10:16 +0400

    my first commiact

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const dotenv = require('dotenv');
 const cors = require("cors");
 const path = require('path');
 const multer = require('multer');
